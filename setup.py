93693cceede3ca1aa38a2304e326f5212473f49d
Matt Carey <matt@authorityspoke.com>
2021-08-11 16:00:17 -0500

    use setup.py instead of setup.cfg

---
+++ setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("readme.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="anchorpoint",
+    version="0.5.2",
+    author="Matt Carey",
+    author_email="matt@authorityspoke.com",
+    description="Text substring selectors for anchoring annotations",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/mscarey/anchorpoint",
+    project_urls={
+        "Bug Tracker": "https://github.com/mscarey/anchorpoint/issues",
+        "Documentation": "https://anchorpoint.readthedocs.io/en/latest/",
+    },
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: Free To Use But Restricted",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+    ],
+    packages=setuptools.find_packages(exclude=["tests"]),
+    install_requires=["marshmallow>=3.2.2"],
+    python_requires=">=3.8",
+)
